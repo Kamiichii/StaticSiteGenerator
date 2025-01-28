313e6fc32066a1d10e6df13d7eda19cb22746266
kamiichii <berkay.kilic.ce@gmail.com>
2025-01-28 13:12:19 +0300

    added markdown to blocks and some tests for it

--- src/extract_markdown_images_or_texts.py
+++ src/extract_markdown_images_or_texts.py
@@ -1,5 +1,6 @@
 import re
 
+
 def extract_markdown_images(text):
     tuples_list = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)       
     return tuples_list
@@ -8,5 +9,17 @@ def extract_markdown_links(text):
     tuples_list = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return tuples_list
 
+def markdown_to_blocks(markdown):
+
+    raw_blocks = markdown.split('\n\n')
+    blocks = []
+
+    for block in raw_blocks:
+        if block != "":
+            blocks.append(block.strip())
+    
+    return blocks
 
 
+    
+     
