776cc2f50dfe492215369d6d6393f40bba4c9092
kamiichii <berkay.kilic.ce@gmail.com>
2025-08-04 22:56:59 -0400

    Added Leafnode class and added tests for it

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,4 +26,22 @@ class HTMLNode:
            and self.props == other.props
        )
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value,None ,props)
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("The leaf has  to have a value")
+
+        if not self.tag:
+            return f"{self.value}"
+        
+        if self.props is not None:
+            props_html = self.props_to_html() 
+            return f"<{self.tag} {props_html}>{self.value}</{self.tag}>"
+        else:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        
+        
         
