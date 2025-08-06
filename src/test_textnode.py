3625b0fe7b15c97313fc7bfe89ace73d285d5171
kamiichii <berkay.kilic.ce@gmail.com>
2025-08-05 23:35:38 -0400

    Added text_node_to_html function and some tests for it

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -19,8 +19,30 @@ class TestTextNode(unittest.TestCase):
     def test_types_not_eq(self):
         node = TextNode("Text",TextType.ITALIC_TEXT,None)
         node2 = TextNode("Text",TextType.PLAIN_TEXT,None)
-        self.assertNotEqual(node,node2)
-        
+        self.assertNotEqual(node,node2)        
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN_TEXT)
+        html_node = TextNode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    def test_text_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        html_node = TextNode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a text node")
+    def test_text_link(self):
+        node = TextNode("This is a text node", TextType.LINK_TEXT,"Insert rickroll url here")
+        html_node = TextNode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node.props,{"href":"Insert rickroll url here"})
+    def test_text_image(self):
+        node = TextNode("This is an anchor text", TextType.IMAGE_TEXT,"Insert rickroll url here")
+        html_node = TextNode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props,{"src":"Insert rickroll url here","alt":"This is an anchor text"}) 
+
 
 
 if __name__ == "__main__":
