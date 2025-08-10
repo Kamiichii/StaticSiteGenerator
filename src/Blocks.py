77608b4e644291b97eecd3bebdfa3754a7f2486a
kamiichii <berkay.kilic.ce@gmail.com>
2025-08-09 23:50:48 -0400

    Created bunch of helper functions and a final function to combine everything in a markdown_to_html_node function

--- src/Blocks.py
+++ src/Blocks.py
@@ -1,4 +1,8 @@
 from enum import Enum
+from htmlnode import HTMLNode,ParentNode
+from split_nodes_delimiter import text_to_textnodes
+from textnode import TextNode,TextType
+import textwrap
 import re
 
 class BlockTypes(Enum):
@@ -21,7 +25,7 @@ def markdown_to_blocks(markdown):
 def block_to_block_type(markdown):
     if re.match(r"^#{1,6}",markdown):
         return BlockTypes.HEADING_BLOCK
-    elif re.match(r"^```.*```$",markdown):
+    elif re.match(r"^```.*```$",markdown,re.DOTALL):
         return BlockTypes.CODE_BLOCK
     elif re.match(r"^>.*?(?:\n>.*?)*$",markdown):
         return BlockTypes.QUOTE_BLOCK
@@ -35,3 +39,90 @@ def block_to_block_type(markdown):
         return BlockTypes.ORDERED_LIST
     else:
         return BlockTypes.PARAGRAPH_BLOCK
+    
+def text_to_child_node(text):
+    text_nodes = text_to_textnodes(text)
+    child_nodes = []
+    for text_node in text_nodes:
+        child_nodes.append(TextNode.text_node_to_html_node(text_node))
+    return child_nodes
+
+def qblock_to_html_node(text):
+    items = text.split("\n")
+    child_nodes = []
+    for item in items:
+        child_nodes.extend(text_to_child_node(item[1:]))
+    return ParentNode(tag="blockquote",children=child_nodes)
+
+def ulist_to_html_node(text):
+    items = text.split("\n")
+    child_nodes = []
+    for item in items:
+        child_nodes.append(ParentNode(tag="li",children=text_to_child_node(item[2:])))
+    return ParentNode(tag="ul",children=child_nodes)
+
+
+def olist_to_html_node(text):
+    items = text.split("\n")
+    child_nodes = []
+    for item in items:
+        child_nodes.append(ParentNode(tag="li",children=text_to_child_node(item[3:])))
+    return ParentNode(tag="ol",children=child_nodes)
+
+        
+
+def heading_to_html_node(markdown):
+    counter = 0
+    for character in markdown:
+        if character == '#':
+            counter += 1
+        else:
+            break
+    if counter > 6:
+        raise ValueError("Heading cant have more than 6 hashes")
+    return ParentNode(tag=f"h{counter}",children=text_to_child_node(markdown[counter:]))
+
+def paragraph_to_html_node(text):
+    lines = text.split("\n")
+    paragraph_text = " ".join(lines)
+    paragraph_text = re.sub(r"\s+", " ", paragraph_text) 
+    child_nodes = text_to_child_node(paragraph_text)
+    return ParentNode(tag="p",children=child_nodes)
+
+def code_to_html_node(text):
+    text_new = text[3:-3]
+    text_new = text_new.lstrip("\n") 
+    text_new = textwrap.dedent(text_new)
+    html_node = TextNode.text_node_to_html_node(TextNode(text_new,TextType.PLAIN_TEXT))
+    outter =  ParentNode("code",[html_node])
+    return ParentNode("pre",[outter])
+
+def block_to_html_node(markdown,block_type):
+    if block_type == BlockTypes.QUOTE_BLOCK:
+        return qblock_to_html_node(markdown)
+    if block_type == BlockTypes.UNORDERED_LIST:
+        return ulist_to_html_node(markdown)
+    if block_type == BlockTypes.ORDERED_LIST:
+        return olist_to_html_node(markdown)
+    if block_type == BlockTypes.HEADING_BLOCK:
+        return heading_to_html_node(markdown)
+    if block_type == BlockTypes.PARAGRAPH_BLOCK:
+        return paragraph_to_html_node(markdown)
+    if block_type == BlockTypes.CODE_BLOCK:
+        return code_to_html_node(markdown)
+    else:
+        raise TypeError("Given block type is not recognized")
+    
+
+    
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    chlidren = []
+    for block in blocks:
+        type_of_block = block_to_block_type(block)
+        chlidren.append(block_to_html_node(block,type_of_block))
+    return ParentNode("div",chlidren)
+
+        
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from Blocks import markdown_to_blocks,block_to_block_type,BlockTypes
+from Blocks import *
 
 
 class TestExtractMarkdownLinksAndImages(unittest.TestCase):
@@ -67,7 +67,85 @@ This is the same paragraph on a new line
     def test_paragraph(self):
         test_func = block_to_block_type("jibobbob tutuppup")
         self.assertEqual(test_func, BlockTypes.PARAGRAPH_BLOCK)
+    def test_paragraphs(self):
+        md = """
+    This is **bolded** paragraph
+    text in a p
+    tag here
 
+    This is another paragraph with _italic_ text and `code` here
 
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+    ```
+    This is text that _should_ remain
+    the **same** even with inline stuff
+    ```
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+    def test_heading_with_inline_formatting(self):
+
+        md = "### Heading with **bold** and _italic_"
+        html = markdown_to_html_node(md).to_html()
+        expected = "<div><h3> Heading with <b>bold</b> and <i>italic</i></h3></div>"
+        self.assertEqual(html,expected)
+    def test_quote_multiline_with_inline(self):
+        md = "> First line\n> Second line with _italic_ text"
+        html = markdown_to_html_node(md).to_html()
+        expected = "<div><blockquote> First line Second line with <i>italic</i> text</blockquote></div>"
+        self.assertEqual(html,expected)
+    def test_unordered_list_with_inline_formatting(self):
+        md = "- **Bold item**\n- Item with `inline code`"
+        html = markdown_to_html_node(md).to_html()
+        expected = "<div><ul><li><b>Bold item</b></li><li>Item with <code>inline code</code></li></ul></div>"
+        self.assertEqual(html,expected)
+    def test_ordered_list_with_inline_formatting(self):
+        md = "1. First _italic_\n2. Second with **bold**"
+        html = markdown_to_html_node(md).to_html()
+        expected = "<div><ol><li>First <i>italic</i></li><li>Second with <b>bold</b></li></ol></div>"
+        self.assertEqual(html,expected)
+    def test_full_markdown_integration(self):
+        md =  """## My **Title**
+
+This is a paragraph with _italic_, **bold**, and `code`.
+
+> Quote line one
+> Quote line two
+
+- First list item
+- Second list item with **bold**
+
+1. Number one
+2. Number two with `code`
+
+```for i in range(3):
+    print(i)
+```"""
+        html = markdown_to_html_node(md).to_html()
+        expected =  ("<div>"
+        "<h2> My <b>Title</b></h2>"
+        "<p>This is a paragraph with <i>italic</i>, <b>bold</b>, and <code>code</code>.</p>"
+        "<blockquote> Quote line one Quote line two</blockquote>"
+        "<ul><li>First list item</li><li>Second list item with <b>bold</b></li></ul>"
+        "<ol><li>Number one</li><li>Number two with <code>code</code></li></ol>"
+        "<pre><code>for i in range(3):\n    print(i)\n</code></pre>"
+        "</div>")
+        self.assertEqual(html,expected)
+   
 if __name__ == "__main__":
     unittest.main()
