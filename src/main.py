821fc8730bfd6e12f8e651b66b43c96d91263c64
kamiichii <berkay.kilic.ce@gmail.com>
2025-08-14 09:42:06 -0400

    Made it so my program can be used in github pages

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 from textnode import TextType,TextNode
 from copier import copy_content_to_public
 from page_generation import generate_pages_recursive
+from sys import argv
 
 
 def main():
+   if (argv[1]):
+      basepath = argv[1]
+   else:
+      basepath = "/"
+
+
    copy_content_to_public("./static")
-   generate_pages_recursive("content","template.html","public")
+   generate_pages_recursive(basepath,"content","template.html","docs")
 
 main()
--- src/page_generation.py
+++ src/page_generation.py
@@ -12,7 +12,7 @@ def extract_title(markdown):
     else:
         return h1[0].strip()
 
-def generate_page(from_path,template_path,dest_path):
+def generate_page(basepath,from_path,template_path,dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as from_file:
         from_file_contents = from_file.read()
@@ -20,19 +20,19 @@ def generate_page(from_path,template_path,dest_path):
         template_contents = template_file.read()
     html = markdown_to_html_node(from_file_contents).to_html()
     title = extract_title(from_file_contents)
-    new_template = template_contents.replace("{{ Title }}",title).replace("{{ Content }}",html)
+    new_template = template_contents.replace("{{ Title }}",title).replace("{{ Content }}",html).replace('href="/',f'href="{basepath}').replace('src="/',f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as dest_file:
       dest_file.write(new_template)
 
-def generate_pages_recursive(dir_path_content,template_path,dest_dir_path):
+def generate_pages_recursive(basepath,dir_path_content,template_path,dest_dir_path):
     
     for name in os.listdir(dir_path_content):
         dir_path = os.path.join(dir_path_content,name)
         dest_path = os.path.join(dest_dir_path,name)
         if os.path.isdir(dir_path):
-            generate_pages_recursive(dir_path,template_path,dest_path)
+            generate_pages_recursive(basepath,dir_path,template_path,dest_path)
         elif re.match(r"(.*?).md$",name):
             dest_path = pathlib.Path(dest_path).with_suffix(".html")
-            generate_page(dir_path, template_path, dest_path)
+            generate_page(basepath,dir_path, template_path, dest_path)
             
